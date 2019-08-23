db1dc9461a7ac3a4966e7121e08bb6eb778cc201
Brian Doane <brianmdoane@gmail.com>
2019-08-23 17:01:46 -0500

    Capturing all three search params to state, refactored handler to work with all inputs

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,36 +3,61 @@ import SearchBar from './SearchBar'
 
 class Home extends Component {
   state = {
-    searchName: '',
-    searchCity: '',
-    searchState: '',
-    searches: [],
+    searchName: {
+      search: '',
+      exists: false,
+    },
+    searchCity: {
+      search: '',
+      exists: false,
+    },
+    searchState: {
+      search: '',
+      exists: false,
+    },
+    searchTerms: [],
+    alert: 'Please add search term to at least one field above.'
   }
 
-  handleName = (e) => {
-    this.setState({
-      searchName: e.target.value
-    })
+  handleTerms = (e) => {
+    const inputValue = e.target.value
+    const term = e.target.name
+    inputValue.length > 0
+      ? this.setState(prevState => ({
+        [term]: {
+          search: inputValue,
+          exists: true
+        }
+      }))
+      : this.setState(prevState => ({
+        [term]: {
+          search: inputValue,
+          exists: false
+        }
+      }))
   }
 
   handleSubmit = (e) => {
     e.preventDefault()
-    this.setState(prevState => ({
-      searches: [prevState.searchName, ...prevState.searches],
-      searchName: ''
-    }))
+    const { searchName, searchCity, searchState, alert } = this.state
+    if (searchName.search === '' && searchCity.search === '' && searchState.search === '') {
+      console.log('alert', alert)
+    }
   }
+
 
   render() {
     console.log('this.state', this.state)
-    const { searchName } = this.state
+    const { searchName, searchCity, searchState } = this.state
 
     return (
       <div>
         <SearchBar
-          handleName={this.handleName}
+          handleTerms={this.handleTerms}
           handleSubmit={this.handleSubmit}
           searchName={searchName}
+          searchCity={searchCity}
+          searchState={searchState}
         />
       </div>
     )
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 
-function SearchBar ({ handleName, handleSubmit, searchName }) {
+function SearchBar ({ handleTerms, handleSubmit, searchName, searchCity, searchState }) {
   return (
     <div className='container search'>
       <Form>
@@ -10,28 +10,30 @@ function SearchBar ({ handleName, handleSubmit, searchName }) {
           <Form.Label>Search by brewery name</Form.Label>
           <Form.Control
             autoFocus
-            type="text"
-            placeholder="Find by name..."
-            aria-label='Find by name'
             type='text'
-            onChange={(e) => handleName(e)}
-            value={searchName}
+            name='searchName'
+            placeholder='Find by name...'
+            aria-label='Find by name'
+            onChange={(e) => handleTerms(e)}
+            value={searchName.search}
           />
           <Form.Label>... by city</Form.Label>
           <Form.Control
-            autoFocus
-            type="text"
-            placeholder="Find by city..."
-            aria-label='Find by city'
             type='text'
+            name='searchCity'
+            placeholder='Find by city...'
+            aria-label='Find by city'
+            onChange={(e) => handleTerms(e)}
+            value={searchCity.search}
           />
           <Form.Label>... by state</Form.Label>
           <Form.Control
-            autoFocus
-            type="text"
-            placeholder="Find by state..."
-            aria-label='Find by state'
             type='text'
+            name='searchState'
+            placeholder='Find by state...'
+            aria-label='Find by state'
+            onChange={(e) => handleTerms(e)}
+            value={searchState.search}
           />
         </Form.Group>
         <Button variant="outline-primary" type="submit" onClick={(e) => handleSubmit(e)}>
