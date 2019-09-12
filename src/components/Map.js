e7de0e369590a6a4b827957b86125a3c5e4ef715
Brian Doane <brianmdoane@gmail.com>
2019-09-12 11:34:58 -0500

    Installed google-maps-react, have map in Brewery component, sizing issue with div from GoogleMapsContainer comp

---
+++ src/components/Map.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react'
+
+const mapStyles = {
+  position: 'absolute',
+  width: '300px',
+  height: '300px',
+  textAlign: 'center'
+}
+
+class GoogleMapsContainer extends Component {
+  render() {
+    const { name, latitude, longitude } = this.props.brewery
+    return (
+        <Map
+          google={this.props.google}
+          zoom={14}
+          style={mapStyles}
+          initialCenter={{
+            lat: latitude,
+            lng: longitude
+            }}
+        >
+          <Marker
+          name={name}
+            position={{ lat: latitude, lng: longitude }} />
+        </Map>
+    )
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBFE7hamTCCRWQCLJtfsPzw3ubzkwNpr0Y'
+})(GoogleMapsContainer);
