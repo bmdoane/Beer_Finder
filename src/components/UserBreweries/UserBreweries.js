74abe0b671700b6f0a7b58118bd58a9febcb3351
Brian Doane <brianmdoane@gmail.com>
2020-01-06 15:25:23 -0600

    Refactored FB brewery notes data. Able to add brewery to user and handle if brewery already exists

--- src/components/UserBreweries/UserBreweries.js
+++ src/components/UserBreweries/UserBreweries.js
@@ -1,21 +1,72 @@
-import React from 'react'
+import React, { Component } from 'react'
+import db from '../../Firebase'
+import { AuthContext } from '../../context/Auth'
 import { Container, LinkWrapper, HomeLink } from './UserBreweries.styles'
+import LoadSpinner from '../../components/UI/Spinner/Spinner'
 
-const UserBreweries = (props) => {
-  const { brewery } = props.location.state;
-  console.log('UB', brewery)
-  return (
-    <Container>
-      <h1>{brewery.name}</h1>
-      <div>Text area for notes:</div>
-      <div>Add a favorite beer:</div>
-      <div>Be able to star rate beers</div>
-      <div>Wishlist beers text input</div>
-      <LinkWrapper>
-        <HomeLink to="/">Find another brewery</HomeLink>
-      </LinkWrapper>
-    </Container>
-  );
+class UserBreweries extends Component {
+  static contextType = AuthContext
+
+  state = {
+    isLoading: true,
+    note: '',
+    notes: [],
+    hasNotes: false,
+    brewery: null,
+    userId: null,
+  }
+
+  componentDidMount() {
+    const { brewery } = this.props.location.state
+    const { currentUser } = this.context
+    // Get to list
+    db.doc(`breweries/${currentUser.uid}`)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          let breweryNotes = doc.data()
+          return breweryNotes[brewery.id]
+        } else {
+          throw new Error("No such document!")
+        }
+      })
+      .then(breweryNotes => {
+        // Have to handle undefined data
+        if (breweryNotes === undefined) {
+          console.log('This brewery has no notes')
+        } else {
+          console.log('breweryNotes', breweryNotes)
+        }
+      })
+      .catch(error => {
+        console.log("Error getting document:", error)
+      })
+    this.setState({
+      isLoading: false,
+      brewery,
+      userId: currentUser.uid,
+    })
+  }
+
+  render() {
+    const { brewery, isLoading, notes } = this.state
+    console.log('brew', brewery)
+    console.log('notes', notes)
+
+    let headline = isLoading ? <LoadSpinner /> : <h1>{brewery.name}</h1>
+    return (
+      <Container>
+        {headline}
+        <div>Text area for notes:</div>
+        <div>Add a favorite beer:</div>
+        <div>Be able to star rate beers</div>
+        <div>Wishlist beers text input</div>
+        <LinkWrapper>
+          <HomeLink to="/">Find another brewery</HomeLink>
+        </LinkWrapper>
+      </Container>
+    )
+  }
 }
 
 export default UserBreweries
--- src/containers/User/User.js
+++ src/containers/User/User.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import db from '../../Firebase'
-import States from 'datasets-us-states-abbr-names'
 import { AuthContext } from '../../context/Auth'
+import States from 'datasets-us-states-abbr-names'
 import { getBrewery } from '../../utils/api'
 import LoadSpinner from '../../components/UI/Spinner/Spinner'
 import { Container, UserContainer, Bio, PortraitIcon, Headline, BreweryLink, BreweryList, LinkWrapper, HomeLink } from './User.styles'
@@ -31,6 +31,7 @@ class User extends Component {
       userName: '',
     },
     userBreweries: [],
+    userId: null,
     isLoading: true,
   }
 
@@ -52,16 +53,42 @@ class User extends Component {
         console.log('data', data)
         this.setState({user: {
           memberSince: dateFormat(memberSince),
-          userName: userName,
+          userName,
         }})
         return getUserBreweries(userBreweries)
       })
-      .then(breweries => this.setState({
-        userBreweries: breweries,
+      .then(userBreweries => this.setState({
+        userBreweries,
+        userId: currentUser.uid,
         isLoading: false,
       }))
       .catch(error => {
         console.log("Error getting document:", error)
+      })
+  }
+
+  addNoteRef = (userId, breweryId) => {
+    db.doc(`breweries/${userId}`)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          let breweryIds = Object.keys(doc.data())
+          let numArr = breweryIds.map(str => Number(str))
+          if (numArr.includes(breweryId)) {
+            return null
+          } else {
+            db.doc(`breweries/${userId}`)
+              .update({
+                [breweryId]: [],
+              })
+              console.log(`breweryNotes added for ${breweryId}`)
+          }
+        } else {
+          db.doc(`breweries/${userId}`)
+            .set({
+              [breweryId]: [],
+            })
+        }
       })
   }
 
@@ -89,11 +116,12 @@ class User extends Component {
               <li key={id}>
                 <BreweryLink
                   to={{
-                    pathname: "/userBreweries",
+                    pathname: `/userBreweries`,
                     state: {
                       brewery: brewery
                     }
-                  }}>
+                  }}
+                  onClick={() => this.addNoteRef(currentUser.uid, id)}>
                   {name} - {city}, {abbrState(state)}
                 </BreweryLink>
               </li>
@@ -112,7 +140,7 @@ class User extends Component {
       <Container>
         {userProfile}
       </Container>
-    );
+    )
   }
 }
 
